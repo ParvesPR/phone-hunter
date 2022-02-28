a21ac36ba3bc39ff400b3a22f8dab8ed9200cc35
ParvesPR <parves2585@gmail.com>
2022-02-28 22:09:46 +0600

    get search result

--- js/app.js
+++ js/app.js
@@ -2,4 +2,27 @@ const loadPhones = () => {
     const inputField = document.getElementById('input-field');
     const inputText = inputField.value;
     inputField.value = '';
+
+    const phoneUrl = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
+    fetch(phoneUrl)
+        .then(res => res.json())
+        .then(phoneData => allPhones(phoneData.data))
+}
+
+const allPhones = phones => {
+    const searchResult = document.getElementById('search-result');
+    for (const phone of phones) {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top w-50 mx-auto p-3" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Brand: ${phone.brand}</h5>
+                  <p class="card-text">${phone.phone_name}</p>
+                </div>
+              </div>
+        `;
+        searchResult.appendChild(div)
+    }
 }
