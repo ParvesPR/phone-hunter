650166de9c19ae482cd965f897a6307a877ba746
ParvesPR <parves2585@gmail.com>
2022-02-28 22:52:54 +0600

    css added

--- js/app.js
+++ js/app.js
@@ -2,15 +2,24 @@ const loadPhones = () => {
     const inputField = document.getElementById('input-field');
     const inputText = inputField.value;
     inputField.value = '';
+    if (inputText == '') {
+        const inputError = document.getElementById('error-input');
+        inputError.innerText = 'Please Search Phone Name';
+    } else {
+        const inputError = document.getElementById('error-input');
+        inputError.innerText = '';
+        const phoneUrl = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
+        fetch(phoneUrl)
+            .then(res => res.json())
+            .then(phoneData => allPhones(phoneData.data))
+    }
 
-    const phoneUrl = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
-    fetch(phoneUrl)
-        .then(res => res.json())
-        .then(phoneData => allPhones(phoneData.data))
+
 }
 
 const allPhones = phones => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     for (const phone of phones) {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -18,11 +27,19 @@ const allPhones = phones => {
         <div class="card h-100">
                 <img src="${phone.image}" class="card-img-top w-50 mx-auto p-3" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">Brand: ${phone.brand}</h5>
-                  <p class="card-text">${phone.phone_name}</p>
+                  <h4 class="card-title">Brand: ${phone.brand}</h4>
+                  <h6 class="card-text">Model: ${phone.phone_name}</h6>
+                  <button onclick="phoneInfo('${phone.slug}')" class="btn bg-success text-white">Phone Info</button>
                 </div>
               </div>
         `;
         searchResult.appendChild(div)
     }
+}
+
+const phoneInfo = info => {
+    const infoUrl = `https://openapi.programming-hero.com/api/phone/${info}`
+    fetch(infoUrl)
+        .then(res => res.json())
+        .then(infoData => console.log(infoData.data))
 }
